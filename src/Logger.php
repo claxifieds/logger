afde18bf0691d638bd0a8521ac9a48f472c498cf
Christopher Silva <chris@planner.net.br>
2020-01-12 13:53:41 +0100

    Add namespace

--- src/Logger.php
+++ src/Logger.php
@@ -1,4 +1,8 @@
-<?php if ( ! defined( 'ABS_PATH' ) ) {
+<?php
+
+namespace Claxifieds\Logger;
+
+if ( ! defined( 'ABS_PATH' ) ) {
 	exit( 'ABS_PATH is not loaded. Direct access is not allowed.' );
 }
 
